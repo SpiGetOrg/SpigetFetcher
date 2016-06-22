9d38d23df349c44caf4e298010541c2437edb953
inventivetalent <inventivetalent@inventivegames.de>
2016-06-23 00:23:43 +0200

    apparently the release date DOES have a date-time attribute, so let's parse that instead

--- src/main/java/org/spiget/fetcher/parser/ResourceListItemParser.java
+++ src/main/java/org/spiget/fetcher/parser/ResourceListItemParser.java
@@ -10,7 +10,6 @@ import org.spiget.data.resource.SpigetIcon;
 import org.spiget.data.resource.version.ListedResourceVersion;
 
 import java.io.IOException;
-import java.util.TimeZone;
 
 import static org.spiget.fetcher.parser.ParserUtil.*;
 
@@ -46,7 +45,7 @@ public class ResourceListItemParser {
 			}
 			{
 				Element resourceReleaseDate = abbrOrSpan(resourceDetails, ".DateTime");// <span class="DateTime" title="May 27, 2016 at 5:20 PM">May 27, 2016</span>
-				listedResource.setReleaseDate(parseDateTimeToLong(resourceReleaseDate.attr("title") + " " + TimeZone.getDefault().getDisplayName()));
+				listedResource.setReleaseDate(Long.parseLong(resourceReleaseDate.attr("data-time")));
 			}
 			{
 				Element resourceCategory = null;
