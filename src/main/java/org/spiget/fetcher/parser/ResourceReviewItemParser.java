96cc468b6cd375531c57a45af23dcabaaa04e2a8
inventivetalent <inventivetalent@inventivegames.de>
2016-07-20 19:16:29 +0200

    update review parser to include the ID

--- src/main/java/org/spiget/fetcher/parser/ResourceReviewItemParser.java
+++ src/main/java/org/spiget/fetcher/parser/ResourceReviewItemParser.java
@@ -2,6 +2,7 @@ package org.spiget.fetcher.parser;
 
 import org.jsoup.nodes.Element;
 import org.spiget.data.author.ListedAuthor;
+import org.spiget.data.resource.Rating;
 import org.spiget.data.resource.ResourceReview;
 import org.spiget.data.resource.SpigetIcon;
 
@@ -12,46 +13,61 @@ import static org.spiget.fetcher.parser.ParserUtil.*;
 public class ResourceReviewItemParser {
 
 	public ResourceReview parse(Element resourceReviewItem) {
-		ResourceReview review = new ResourceReview();
+		int reviewId;
+		ListedAuthor reviewAuthor;
+		Rating reviewRating;
+		String reviewVersion;
+		String reviewMessage;
+		long reviewDate;
+		String reviewResponse = null;
 
 		{
 			Element memberAvatar = resourceReviewItem.select("a.avatar").first();
-			ListedAuthor reviewAuthor = new ListedAuthor(Integer.parseInt(extractIdFromUrl(memberAvatar.attr("href"), DOT_URL_ID)));
+			reviewAuthor = new ListedAuthor(Integer.parseInt(extractIdFromUrl(memberAvatar.attr("href"), DOT_URL_ID)));
 			reviewAuthor.setName(resourceReviewItem.attr("data-author"));
 			SpigetIcon avatar = new IconParser().parse(memberAvatar);
 			reviewAuthor.setIcon(avatar);
-
-			review.setAuthor(reviewAuthor);
 		}
 		{
 			Element messageInfo = resourceReviewItem.select("div.messageInfo").first();
 			{
 				Element rating = messageInfo.select("div.rating").first();
-				review.setRating(new RatingParser().parseSingle(rating));
+				reviewRating = new RatingParser().parseSingle(rating);
 			}
 			{
 				Element version = abbrOrSpan(messageInfo, ".muted");
-				String versionName = version.text().substring("Version: ".length());
-				review.setVersion(versionName);
+				reviewVersion = version.text().substring("Version: ".length());
 			}
 			{
 				Element baseHtml = messageInfo.select("blockquote.baseHtml").first();
-				review.setMessage(Base64.getEncoder().encodeToString(baseHtml.html().getBytes()));
+				reviewMessage = Base64.getEncoder().encodeToString(baseHtml.html().getBytes());
 			}
 
 			{
 				Element messageMeta = messageInfo.select("div.messageMeta").first();
-				review.setDate(parseTimeOrTitle(abbrOrSpan(messageMeta, ".DateTime")));
+
+				Element permalink = messageMeta.select("a.item").first();
+				reviewId = Integer.parseInt(extractIdFromUrl(permalink.attr("href"), PARAM_URL_ID));
+
+				reviewDate = parseTimeOrTitle(abbrOrSpan(messageMeta, ".DateTime"));
 			}
 
 			{
 				Element messageResponse = messageInfo.select("ol.messageResponse").first();
 				if (messageResponse != null) {
 					Element blockQuote = messageResponse.select("blockquote").first();
-					review.setResponseMessage(Base64.getEncoder().encodeToString(blockQuote.html().getBytes()));
+					reviewResponse = Base64.getEncoder().encodeToString(blockQuote.html().getBytes());
 				}
 			}
 		}
+
+		ResourceReview review = new ResourceReview(reviewId);
+		review.setAuthor(reviewAuthor);
+		review.setRating(reviewRating);
+		review.setVersion(reviewVersion);
+		review.setMessage(reviewMessage);
+		review.setDate(reviewDate);
+		review.setResponseMessage(reviewResponse);
 
 		return review;
 	}
